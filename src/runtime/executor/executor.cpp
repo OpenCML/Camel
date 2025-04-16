79d840f8a594e72e7b83df0688c7e7cca966f267
Eafoo <ychennc@connect.ust.hk>
2025-04-17 00:02:12 +0800

    feat: Executor 初步框架功能实现

---
+++ src/runtime/executor/executor.cpp
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2024 the OpenCML Organization
+ * Camel is licensed under the MIT license.
+ * You can use this software according to the terms and conditions of the
+ * MIT license. You may obtain a copy of the MIT license at:
+ * [https://opensource.org/license/mit]
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ * KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the the MIT license for more details.
+ *
+ * Author: Zhenjie Wei
+ * Created: Apr. 16, 2025
+ * Updated: Apr. 16, 2025
+ * Supported by: National Key Research and Development Program of China
+ */
+
+#include "executor.h"
+#include "executors/InnerExecutor.h"
+
+void ExecutorManager::registerExecutorCreator(std::string name, ExecutorManager::creator_t creator) {
+    executorCreators.emplace(name, std::move(creator));
+}
+
+ExecutorManager::ExecutorManager() {
+    registerExecutorCreator("inner", []() { return std::make_unique<InnerExecutor>(); });
+}
+
+Status ExecutorManager::executeOperator(std::string uri, std::vector<data_ptr_t> withArgs, std::vector<data_ptr_t> normArgs, data_ptr_t &ret) {
+    const size_t pos = uri.find("://");
+    if (pos == std::string::npos) {
+        return { RetCode::invalidURI, "Invalid URI format" };
+    }
+    const std::string protocol = uri.substr(0, pos);
+    auto it_loaded = loadedExecutors.find(protocol);
+    if (it_loaded != loadedExecutors.end()) {
+        return it_loaded->second->execute(uri.substr(pos + 3), withArgs, normArgs, ret);
+    }
+    auto it_creator = executorCreators.find(protocol);
+    if (it_creator == executorCreators.end()) {
+        return { RetCode::invalidURI, "Unregistered protocol: " + protocol };
+    }
+    auto executor = it_creator->second(); 
+    Status status = executor->execute(uri.substr(pos + 3), withArgs, normArgs, ret);
+    loadedExecutors.emplace(protocol, std::move(executor));
+    return status;
+}
+
+Status BaseExecutor::execute(std::string uri, std::vector<data_ptr_t> withArgs, std::vector<data_ptr_t> normArgs, data_ptr_t &ret) {
+    return { RetCode::unknownError, "BaseExecutor: No implementation for execute" };
+}
---
+++ src/runtime/executor/executor.h
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2024 the OpenCML Organization
+ * Camel is licensed under the MIT license.
+ * You can use this software according to the terms and conditions of the
+ * MIT license. You may obtain a copy of the MIT license at:
+ * [https://opensource.org/license/mit]
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ * KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the the MIT license for more details.
+ *
+ * Author: Zhenjie Wei
+ * Created: Apr. 16, 2025
+ * Updated: Apr. 16, 2025
+ * Supported by: National Key Research and Development Program of China
+ */
+
+#pragma once
+
+#include "../common/type.h"
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <functional>
+#include <memory>
+
+enum class RetCode { 
+    success, 
+    invalidWithParameter, 
+    invalidnormParameter,
+    invalidURI,
+    errorOnExecution,
+    unknownError
+};
+
+struct Status {
+    RetCode code;
+    std::string errorMessage;
+};
+
+class BaseExecutor {
+  public:
+    std::string executorName = "";
+    virtual Status execute(std::string uri, std::vector<data_ptr_t> withArgs, std::vector<data_ptr_t> normArgs, data_ptr_t &ret);
+    virtual ~BaseExecutor() = default;
+};
+
+class ExecutorManager {
+  private:
+    using creator_t = std::function<std::unique_ptr<BaseExecutor>()>;
+    std::unordered_map<std::string, std::unique_ptr<BaseExecutor>> loadedExecutors;
+    std::unordered_map<std::string, creator_t> executorCreators;
+    void registerExecutorCreator(std::string name, creator_t creator);
+  public:
+    Status executeOperator(std::string uri, std::vector<data_ptr_t> withArgs, std::vector<data_ptr_t> normArgs, data_ptr_t &ret);
+    ExecutorManager();
+    ~ExecutorManager() = default;
+};
