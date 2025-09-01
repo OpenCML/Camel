80355623c7fbdf3ef0cb028cf3ec5bf41105eccd
Zhenjie Wei <Kogler_S@outlook.com>
2025-09-01 19:08:12 +0800

    style: 代码格式化

--- src/common/module/userdef.cpp
+++ src/common/module/userdef.cpp
@@ -37,8 +37,7 @@ using namespace std;
 using namespace antlr4;
 
 UserDefinedModule::UserDefinedModule(
-    const std::string &name, const std::string &path, context_ptr_t ctx,
-    parser_ptr_t parser)
+    const std::string &name, const std::string &path, context_ptr_t ctx, parser_ptr_t parser)
     : Module(name, path), context_(ctx) {
     if (parser) {
         parser_ = parser;
